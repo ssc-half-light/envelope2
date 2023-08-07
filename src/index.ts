ac89107128df1d3c81ec03a600ddddfbf453d8d8
nick thomas <nichoth@gmail.com>
2023-08-06 20:59:44 -0700

    tests pass

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import { fromString, toString } from 'uint8arrays'
 import { Crypto } from '@oddjs/odd'
 import { SymmAlg } from 'keystore-idb/types.js'
 import { writeKeyToDid } from '@ssc-hermes/util'
+import * as ed from '@noble/ed25519'
 import {
     aesGenKey,
     aesEncrypt,
     aesDecrypt,
-    // rsaDecrypt
 } from '@oddjs/odd/components/crypto/implementation/browser'
 import { Identity, encryptKey, createDeviceName } from '@ssc-hermes/identity'
 import serialize from 'json-canon'
+
+// map of device name to encrypted key string
+type Keys = Record<string, string>
 
 // {
 //     seq: 0,
@@ -22,14 +25,22 @@ import serialize from 'json-canon'
 
 export const ALGORITHM = SymmAlg.AES_GCM
 
-export type Envelope = SignedRequest<{
+interface UnsignedEnvelope {
     seq:number,
     expiration:number,
-    recipient:string,  // the recipient's username
-}>
+    publicKey:string,  // ed25519 public key, encoded as base64pad
+    recipient:string,  // the recipient's username (the person creating the envelope)
+    encrypted?:{ key:Keys, content:string }  /* can include a private key,
+        encrypted to someone. `content` is the encrypted private key */
+}
 
-// map of device name to encrypted key string
-type Keys = Record<string, string>
+/**
+ * An 'envelope'
+ * Can include an 'encrypted' portion, which would include the private side
+ *   of the given public key. In this case, the envelope would be for a specific
+ *   person who is able to decrypt it.
+ */
+export type Envelope = SignedRequest<UnsignedEnvelope>
 
 type Content = SignedRequest<{
     from:{ username:string },
@@ -42,45 +53,140 @@ export interface EncryptedContent {
     content:string
 }
 
-/**
- * Encrypt a string and put it into an envelope. The envelope tells us who the
- * recipient of the message is; the message sender is hidden.
- * @param me Your Identity.
- * @param recipient The identity of the recipient, because we need to encrypt
- * the message to the recipient.
- * @param envelope The envelope we are putting it in
- * @param content The content that will be encrypted to the recipient
- * @returns [message, <sender's keys>]
- * Return an array of [message, keys], where keys is a map of the sender's devices
- * to the symmetric key encrypted to that device. This is returned as a seperate
- * object because we *don't* want the sender device names to be in the message.
- */
-export async function wrapMessage (
-    me:Identity,
-    recipient:Identity,  // because we need to encrypt the message to the recipient
-    envelope:Envelope,
-    content:Content
-):Promise<[{
-    envelope:Envelope,
-    message:EncryptedContent
-}, Keys]> {
-    // encrypt the content *to* the recipient,
-    // use an Identity to get the exchange keys of all the devices
-    //   of the recipient
+export interface EnvelopedMessage {
+    message:EncryptedContent,
+    signature:string,
+    envelopeKey:string
+}
 
-    // create a key
+/**
+ * Put a message into the envelope. This means use the corresponding private key
+ *   to sign the given message.
+ *
+ * You must get the private key somehow. One option is to pass in the envelope
+ * user when you create the envelope. In which case the corresponding private
+ * key will be encrypted to the envelope user and embedded with the envelope.
+ * @param me Your identity
+ * @param recipient The recipient, because we need to encrypt data *to* them.
+ * @param envelope The envelope you are putting a message into
+ * @param content The content you are putting into the envelope
+ * @param {{ crypto?, privKey? }} opts A crypto or private key to use to sign
+ * the envelope. If `privKey` is omitted, then you must pass a `crypto` instance,
+ * so you can decrypt the embedded private key
+ * @returns {{ message, signature, envelopeKey }} The sealed envelope is the
+ * message along with a signature produced by the envelope's private key. You
+ * would verify the return signature is ok with the `envelopeKey` and the
+ * `message`.
+ */
+export async function sealEnvelope (
+    me:Identity,
+    recipient:Identity,
+    envelope:Envelope,
+    content:Content,
+    { crypto, privKey }:{ crypto?:Crypto.Implementation, privKey?:string }
+):Promise<EnvelopedMessage> {
     const key = await aesGenKey(ALGORITHM)
-    // encrypt the key to the recipient,
-    // also encrypt the content with the key
     const encryptedContent = await encryptContent(key, serialize(content),
         recipient)
 
-    return [
-        {
-            envelope,
-            message: encryptedContent,
-        },
-        await encryptKeys(me, key)]
+    // const encryptedContent = await encryptContent(key, serialize({
+    //     content,
+    //     from: {
+    //         username: me.username,
+    //         rootDid: me.rootDid
+    //     }
+    // }), recipient)
+
+    const contentForSigning:Uint8Array = fromString(serialize(encryptedContent), 'utf8')
+
+    let _privKey
+    // if not passed in, the priv key should be embedded in the envelope
+    if (!privKey) {
+        if (!crypto) throw new Error('not crypto and not privKey')
+        if (!envelope.encrypted) throw new Error('not privKey and not encrypted')
+        // const { privateKey } = await decryptMessage(crypto, envelope.encrypted)
+        // _privKey = await decryptMessage(crypto, envelope.encrypted)
+        // _privKey = privateKey
+        _privKey = await getKeyFromEnvelope(crypto, envelope.encrypted)
+    }
+
+    const signature = await ed.signAsync(
+        contentForSigning,
+        (privKey ? arrFromString(privKey) : _privKey)
+    )
+
+    return {
+        message: encryptedContent,
+        signature: toString(signature, 'base64pad'),
+        envelopeKey: envelope.publicKey
+    }
+}
+
+// /**
+//  * Encrypt a string and put it into an envelope. The envelope tells us who the
+//  * recipient of the message is; the message sender is hidden.
+//  * @param me Your Identity.
+//  * @param recipient The identity of the recipient, because we need to encrypt
+//  * the message to the recipient.
+//  * @param envelope The envelope we are putting it in
+//  * @param content The content that will be encrypted to the recipient
+//  * @returns [message, <sender's keys>]
+//  * Return an array of [message, keys], where keys is a map of the sender's devices
+//  * to the symmetric key encrypted to that device. This is returned as a seperate
+//  * object because we *don't* want the sender device names to be in the message.
+//  */
+// export async function wrapMessage (
+//     me:Identity,
+//     recipient:Identity,  // because we need to encrypt the message to the recipient
+//     envelope:Envelope,
+//     content:Content
+// ):Promise<[{
+//     envelope:Envelope,
+//     message:EncryptedContent
+// }, Keys]> {
+//     // encrypt the content *to* the recipient,
+
+//     // create a key
+//     const key = await aesGenKey(ALGORITHM)
+//     // encrypt the key to the recipient,
+//     // also encrypt the content with the key
+//     const encryptedContent = await encryptContent(key, serialize(content),
+//         recipient)
+
+//     return [
+//         {
+//             envelope,
+//             message: encryptedContent,
+//         },
+//         await encryptKeys(me, key)]
+// }
+
+/**
+ * Get a private key that is embedded in the envelope. That means this envelope
+ * was created specifically for you.
+ * @param crypto
+ * @param param1
+ * @returns {Promise<Uint8Array>}
+ */
+async function getKeyFromEnvelope (
+    crypto:Crypto.Implementation,
+    { key, content }:{ key:Keys, content:string }  // <- envelope.encrypted
+):Promise<Uint8Array> {
+    const did = await writeKeyToDid(crypto)
+    const deviceName = await createDeviceName(did)
+    const encryptedKey = key[deviceName]
+    const decryptedKey = await crypto.keystore.decrypt(
+        fromString(encryptedKey, 'base64pad')
+    )
+
+    const decrypted = await aesDecrypt(
+        fromString(content, 'base64pad'),
+        decryptedKey,
+        ALGORITHM
+    )
+
+    const { privateKey } = (JSON.parse(new TextDecoder().decode(decrypted)))
+    return arrFromString(privateKey)
 }
 
 export async function decryptMessage (
@@ -89,9 +195,7 @@ export async function decryptMessage (
 ):Promise<Content> {
     const did = await writeKeyToDid(crypto)
     const deviceName = await createDeviceName(did)
-
     const encryptedKey = msg.key[deviceName]
-
     const decryptedKey = await crypto.keystore.decrypt(
         fromString(encryptedKey, 'base64pad')
     )
@@ -113,20 +217,40 @@ export async function decryptMessage (
  *   seq: an always incrementing integer
  *   expiration: timestamp to expire, default is 1 year from now
  * the sequence number to use for this envelope
- * @returns {Promise<Envelope>} A serizlizable certificate
+ * @returns {Promise<Envelope>} A serializable certificate
  */
 export async function create (crypto:Crypto.Implementation, {
+    envelopeUser,  // who will be using this envelope
     username,
     seq,
     // expire 1 year from now by default
     expiration = new Date().setFullYear(new Date().getFullYear() + 1)
-}:{ username:string, seq:number, expiration?:number }):Promise<Envelope> {
-    const envelope = await createMsg(crypto, {
+}:{
+    envelopeUser?:Identity,
+    username:string,
+    seq:number,
+    expiration?:number
+}):Promise<Envelope> {
+    const privKey = ed.utils.randomPrivateKey()
+    const pubKey = await ed.getPublicKeyAsync(privKey)
+    const pubKeyStr = toString(pubKey, 'base64pad')
+
+    const preEnvelope:UnsignedEnvelope = {
         seq,
         expiration,
+        publicKey: pubKeyStr,
         recipient: username  // our username goes on the envelope
-    })
+    }
+    if (envelopeUser) {
+        // if we know who will be using this envelope,
+        // then encrypt the corresponding private key to them
+        const aesKey = await aesGenKey(ALGORITHM)
+        preEnvelope.encrypted = await encryptContent(aesKey, serialize({
+            privateKey: toString(privKey, 'base64pad')
+        }), envelopeUser)
+    }
 
+    const envelope = await createMsg(crypto, preEnvelope)
     return envelope
 }
 
@@ -134,6 +258,7 @@ export async function create (crypto:Crypto.Implementation, {
  * Take data in string format, and encrypt it with the given symmetric key.
  * @param key The symmetric key used to encrypt/decrypt
  * @param data The text to encrypt
+ * @param recipient The `Identity` redord for the recipient.
  * @returns {Promise<{ key:Keys, content:string }>}
  */
 export async function encryptContent (
@@ -142,7 +267,7 @@ export async function encryptContent (
     recipient:Identity
 ):Promise<{ key:Keys, content:string }> {
     const encrypted = arrToString(await aesEncrypt(
-        new TextEncoder().encode(data),
+        new TextEncoder().encode(data.normalize()),
         key,
         ALGORITHM
     ))
@@ -158,7 +283,7 @@ export async function encryptContent (
 /**
  * Take a given AES key and encrypt it to all the devices in the given identity.
  * @param id The identity we are encrypting to
- * @param key The key we are encrypting
+ * @param key The AES key we are encrypting
  * @returns {Record<string, string>}
  */
 async function encryptKeys (id:Identity, key:CryptoKey):
@@ -172,10 +297,10 @@ Promise<Keys> {
     return encryptedKeys
 }
 
-function arrFromString (str:string) {
+export function arrFromString (str:string) {
     return fromString(str, 'base64pad')
 }
 
-function arrToString (arr:Uint8Array) {
+export function arrToString (arr:Uint8Array) {
     return toString(arr, 'base64pad')
 }
--- test/index.ts
+++ test/index.ts
@@ -1,24 +1,35 @@
 import { test } from '@socketsupply/tapzero'
 import { create as createId, Identity } from '@ssc-hermes/identity'
 import { createCryptoComponent } from '@ssc-hermes/node-components'
+import * as ed from '@noble/ed25519'
+import serialize from 'json-canon'
+import { fromString } from 'uint8arrays'
 import { Crypto } from '@oddjs/odd'
 import {
     create as createEnvelope,
     Envelope,
-    wrapMessage,
+    // wrapMessage,
+    arrFromString,
+    sealEnvelope,
     EncryptedContent,
-    decryptMessage
+    decryptMessage,
+    EnvelopedMessage
 } from '../dist/index.js'
 import { create as createMsg } from '@ssc-hermes/message'
 
 let alicesEnvelope:Envelope
 let alice:Identity
 let alicesCrypto:Crypto.Implementation
+// let bob:Identity
+// let bobsCrypto
+const bobsCrypto = await createCryptoComponent()
+const bob = await createId(bobsCrypto, { humanName: 'bob' })
 
-test('create an envelope', async t => {
+test('create an envelope to alice', async t => {
     alicesCrypto = await createCryptoComponent()
     alice = await createId(alicesCrypto, { humanName: 'alice' })
     alicesEnvelope = await createEnvelope(alicesCrypto, {
+        envelopeUser: bob,
         username: alice.username,
         seq: 0
     })
@@ -30,35 +41,54 @@ test('create an envelope', async t => {
 
 // let alicesKeys:Record<string, string>
 let msgContent:EncryptedContent
-let bob:Identity
+let envelopedMsg:EnvelopedMessage
+// let bob:Identity
 
-test('put a message in the envelope', async t => {
-    const bobsCrypto = await createCryptoComponent()
-    bob = await createId(bobsCrypto, { humanName: 'bob' })
+test('bob puts a message in the envelope to alice', async t => {
+    // bobsCrypto = await createCryptoComponent()
+    // bob = await createId(bobsCrypto, { humanName: 'bob' })
 
     const content = await createMsg(bobsCrypto, {
         from: { username: bob.username },
         text: 'hello'
     })
 
-    const [
-        { envelope: returnedEnvelope, message },  // the encrypted message content
-        keys  // map of sender's device name to encrypted key string
-    ] = await wrapMessage(bob, alice, alicesEnvelope, content)
+    // console.log('***content***', content)
 
+    // const [
+    //     { envelope: returnedEnvelope, message },  // the encrypted message content
+    //     keys  // map of sender's device name to encrypted key string
+    // ] = await wrapMessage(bob, alice, alicesEnvelope, content)
+
+    envelopedMsg = await sealEnvelope(bob, alice, alicesEnvelope, content, {
+        crypto: bobsCrypto
+    })
+
+    const { message } = envelopedMsg
     msgContent = message
 
-    t.ok(returnedEnvelope, 'should return the envelope')
-    t.equal(returnedEnvelope.signature, alicesEnvelope.signature,
-        'the envelope we get back shoud be equal to what was passed in')
-    t.ok(message, 'should return the encrypted content')
-    t.ok(keys, 'should return keys')
+    t.ok(envelopedMsg, 'should return the envelope')
+    t.ok(envelopedMsg.signature, 'should have a signature on the envelope')
+    // t.equal(envelopedMsg.signature, alicesEnvelope.signature,
+    //     'the envelope we get back shoud be equal to what was passed in')
+    // t.ok(message, 'should return the encrypted content')
+    // t.ok(keys, 'should return keys')
 
     // console.log('**msg**', message)
 })
 
+test('an anonymous node can verify the message is ok', async t => {
+    const msg = fromString(serialize(msgContent), 'utf8')
+    const sig = fromString(envelopedMsg.signature, 'base64pad')
+    const pubKey = arrFromString(envelopedMsg.envelopeKey)
+    // const isValid = await ed.verifyAsync(signature, message, pubKey);
+    const isOk = await ed.verifyAsync(sig, msg, pubKey)
+    t.equal(isOk, true, 'should verify a valid envelope')
+})
+
 test('alice can decrypt a message addressed to alice', async t => {
     const decrypted = await decryptMessage(alicesCrypto, msgContent)
+    console.log('***decrypted***', decrypted)
     t.equal(decrypted.from.username, bob.username,
         "should have bob's username in decrypted message")
     t.equal(decrypted.text, 'hello', 'should have the original text of the message')
